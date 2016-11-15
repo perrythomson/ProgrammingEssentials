ca0598c816b4c34d0ef2e4393d9175dc2094759c
perrythomson <perrythomson@yahoo.com>
2016-11-15 13:48:39 -0700

    Initial commit

---
+++ src/main/java/com/programmingessentials/ProgrammingEssentialsApplication.java
@@ -0,0 +1,12 @@
+package com.programmingessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingEssentialsApplication.class, args);
+	}
+}
